ac52b03ef5543a871e228c16707297483dd1605e
Elena <36563788+lenakrukova@users.noreply.github.com>
2018-03-17 15:46:49 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,8 @@
-
+PI = 3.14    
+def calculate(R, L):
+   volume = PI * R * R * L 
+   return ('Volume: ' +  str(volume))    
+   
+radius = float(input('Please Enter the radius of a Cylinder: '))
+length = float(input('Please Enter the length of a Cylinder: '))
+print(calculate(radius, length))
