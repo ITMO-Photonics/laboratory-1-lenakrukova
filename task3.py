c3ec874ae468422d3e4db686b06e04bb6e489fe8
Elena <36563788+lenakrukova@users.noreply.github.com>
2018-03-17 16:08:40 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,12 @@
+import numpy as np
+import scipy.linalg as lng
 
+N=5
+A = np.zeros((N,N))
+i,j = np.indices(A.shape)
+A[i==j]=1
+A[i==j+1]=1
+A[i==j+2]=1
+B = np.arange(N)
+F = np.linalg.solve (A,B)
+print(F)
